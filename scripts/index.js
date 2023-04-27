ee01290b19e71473025b46ac8eb1aaf8cf61ffc7
Vyacheslav Morev <gromd96@gmail.com>
2023-04-27 19:28:26 +0300

    add form

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const menu = document.querySelector('.menu');
 const buttonMenu = menu.querySelector('.menu__burger');
 const menuLinks = menu.querySelector('.menu__links');
 const menuLink = menu.querySelectorAll('.menu__link');
+const formSubmitButton = document.querySelector('.wedding-wish-guests__form-submit-button');
 const settingsValidation = {
   formSelector: '.wedding-wish-guests__form',
   inputSelector: '.wedding-wish-guests__form-input',
@@ -46,3 +47,54 @@ const enableValidation = (config) => {
 enableValidation(settingsValidation);
 formValidators['wedding-form'].resetValidation();
 
+const URL_APP = 'https://script.google.com/macros/s/AKfycbw9nHrFRNalNPp-3_-IHz87loGFYxqXDCj2Pk_HZ9-26HKuAcEag0kepZmbedKLrisf/exec';
+
+const form = document.querySelector('#form');
+
+form.action = URL_APP;
+
+form.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+
+  const name = document.querySelector('.wedding-wish-guests__text_input_name');
+  const holly = document.querySelector('.wedding-wish-guests__text_input_holly');
+  const car = document.querySelector('.wedding-wish-guests__text_input_car');
+  const eat = document.querySelector('.wedding-wish-guests__text_input_eat');
+  const comment = document.querySelector('.wedding-wish-guests__text_input_comment');
+
+  let details = {
+    name: name.value.trim(),
+    holly: holly.value.trim(),
+    car: car.value.trim(),
+    eat: eat.value.trim(),
+    comment: comment.value.trim(),
+  }
+
+  let formBody = [];
+  for (let property in details) {
+    let encodedKey = encodeURIComponent(property);
+    let encodedValue = encodeURIComponent(details[property]);
+    formBody.push(encodedKey + '=' + encodedValue);
+  }
+
+  formBody = formBody.join('&');
+
+  const result = await fetch(URL_APP, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+    cors: 'no-cors',
+    body: formBody,
+  })
+    .then((res) => res.json());
+  
+  if (result.type === 'success') {
+    name.value = '';
+    holly.value = '';
+    car.value = '';
+    eat.value = '';
+    comment.value = '';
+    alert('chek');
+  }
+})
