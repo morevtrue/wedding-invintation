1baaf58ecde2ee24b69ae5af4e2eecf5f80c0769
Vyacheslav Morev <gromd96@gmail.com>
2023-04-27 12:53:10 +0300

    add fall out menu

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,9 @@
 import { FormValidator } from "./FormValidator.js";
 
+const menu = document.querySelector('.menu');
+const buttonMenu = menu.querySelector('.menu__burger');
+const menuLinks = menu.querySelector('.menu__links');
+const menuLink = menu.querySelectorAll('.menu__link');
 const settingsValidation = {
   formSelector: '.wedding-wish-guests__form',
   inputSelector: '.wedding-wish-guests__form-input',
@@ -7,6 +11,23 @@ const settingsValidation = {
   inactiveButtonClass: 'wedding-wish-guests__form-submit-button_inactive',
   inputErrorClass: 'wedding-wish-guests__text_type_error',
   errorClass: 'wedding-wish-guests__form-error_active',
+};
+
+// ПЕРЕКЛЮЧЕНИЕ МЕНЮ--------------------------------
+buttonMenu.addEventListener('click', () => {
+  switchMenu();
+});
+
+menuLink.forEach(link => {
+  link.addEventListener('click', () => {
+    switchMenu();
+  })
+});
+
+const switchMenu = () => {
+  menuLinks.classList.toggle('menu__links_opened');
+  buttonMenu.classList.toggle('menu__burger_arrow_top');
+  menu.classList.toggle('menu_theme_opacity');
 };
 
 // ВАЛИДАЦИЯ----------------------------------------
